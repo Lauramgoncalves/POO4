50cac7758e028f7c52717d220ee2d43a057d311b
Laura <lauramota2015@hotmail.com>
2023-01-01 20:38:28 -0300

    POO4

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO4
+{
+    class Program
+    {  /*Gerenciamento de Memória
+        Diferença entre struct e classe
+         Servem para definir novos tipos de dados
+        Diferença
+        *Não tem como usar HERANÇA, INTERFACE, SOBREPOSIÇÃO,POLIMOFIRSMO com Struct.
+        *Sempre ue se trabalha com STRUCT, se trabalha com copia de dado na memória, consome mais.
+        *Class faz referencia ou apontamento na memória, sem copiar os dados, se o valor da VAR for repetido, não copia os dados, faz o apontamento, é como se fosse um endereço.
+        *GC ou garbage collector -> se a variavel deixar de existir, automaticamente o GC joga fora, só funciona com Objetos.
+        *As structs são removidas da mémória por um processo de "referenciação" */
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
